e36296d098dc59c70f67ce3248782d781d139091
mrgodem <windows10work@gmail.com>
2016-06-16 09:58:09 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -246,7 +246,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {(173008198)},
     admin_users = {},
     disabled_channels = {}
   }
